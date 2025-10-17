d670750a60d99c37d9c4d03fc75d442c8102a95c
Scott <Scott.Wood@TScottWood.com>
2025-10-17 15:00:52 -0500

    seventeenth

---
+++ index.js
@@ -0,0 +1,13 @@
+template_dn3adwn
+service_jv2i52f
+cPWNN_J6WG_Nmt64A
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init({
+        publicKey: "YOUR_PUBLIC_KEY",
+      });
+   })();
+</script>
