74f81979626ae5d5958bb5b3eb058f57acae2ffa
Scott <Scott.Wood@TScottWood.com>
2025-10-20 18:10:55 -0500

    twentyith

--- index.js
+++ index.js
@@ -4,21 +4,34 @@
 
 function contact(event) {
   event.preventDefault();
-  //    emailjs
-  //    .sendForm(
-  //       'service_jv2i52f',
-  //       'template_dn3adwn',
-  //       event.target,
-  //       '-SS9uQZPgjIEZa4Sx'
-  //    ).then(() => {
-  //    console.log('this worked1')
-  // })
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
-  setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible");
-    success.classList += " modal__overlay--visible";
-    console.log("it worked 1");
-  }, 1000);
+  emailjs
+    .sendForm(
+      "service_jv2i52f",
+      "template_dn3adwn",
+      event.target,
+      "-SS9uQZPgjIEZa4Sx"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable.  Please contact me directly on Scott.Wood@TScottWood.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+   isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
